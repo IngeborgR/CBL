c982fba418554075c207bb7d84c9c992d3e3362b
IngeborgR <IngeborgR@users.noreply.github.com>
2019-03-25 13:05:57 +0100

    Latest versions

--- run.sh
+++ run.sh
@@ -5,10 +5,10 @@ main() {
   # To run the reconstruction task discarding utterances containing new words,
   # set keep_all_variants="0". To always keep such utterances, set to "1", and
   # set to "0 1" to do both.
-  local keep_all_variants="0" #"0 1"
-  local types="c" #"l c"
-  local children="Alex" #"Alex Ethan Lily Naima Violet William"
-  local ages="1_0 1_6 2_0 2_6 3_0 3_6 4_0"
+local keep_all_variants="1" #"0 1"
+local types="c" #"l c"
+local children="Ethan" #"Alex Ethan Lily Naima Violet William"
+local ages="1_0" #"1_0 1_6 2_0 2_6 3_0 3_6 4_0"
 
   for keep_all_variant in $keep_all_variants; do
     for type in $types; do
